2bd3d9309486d731a97d59fa592863787d45ab8c
hsiaosiyuan <hsiaosiyuan0@outlook.com>
2018-12-13 11:00:17 +0800

    proj init

---
+++ swiftbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftbox
+//
+//  Created by hsiaosiyuan on 2018/12/13.
+//  Copyright © 2018 hsiaosiyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
